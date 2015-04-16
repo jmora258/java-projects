3c7ffe117fa47addfbfaf5c4bf4921e829f9e69c
jmora258 <jmora258@ucla.edu>
2015-04-16 00:39:01 -0700

    Add Program description

--- QuadraticSolver/QuadraticSolver.java
+++ QuadraticSolver/QuadraticSolver.java
@@ -1,6 +1,13 @@
 /* Author: 			Justino Mora
  * Project Name: 	Quadratic Solver
- * 
+ * Description: 	This simple Java program solves a quadratic equation using
+ *					Newton's method (Newton-Raphson Method), an iterative procedure
+ *					for finding a root (zero) of a function. 
+ *					Functionality: 
+ * 						- finds one of the roots of your equation starting from
+ * 							your initial guess (must be hard-coded). 
+ * 						- for a better approximation to the actual root, adjust 
+ * 							the tolerance variable (larger number: more accurate)
 */ 
 
 
@@ -12,7 +19,6 @@ public class QuadraticSolver {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello world!"); 
-		System.out.println("Test!"); 
+
 	}
 }
