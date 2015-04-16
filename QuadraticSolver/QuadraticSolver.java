81b78ceea533e27cf0a77c34a4bd4edcf1a9a9a1
jmora258 <jmora258@ucla.edu>
2015-04-16 00:24:39 -0700

    Create QuadraticSolver project to test java at command prompt

---
+++ QuadraticSolver/QuadraticSolver.java
@@ -0,0 +1,17 @@
+/* Author: 			Justino Mora
+ * Project Name: 	Quadratic Solver
+ * 
+*/ 
+
+
+
+public class QuadraticSolver {
+	/**
+     * args the command line arguments
+     */
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world!"); 
+	}
+}
