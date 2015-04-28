afc6d82cff7db1854ec908ea2746f609842185de
jmora258 <jmora258@ucla.edu>
2015-04-28 11:17:13 -0700

    Implement the Constructors and the methods: getEl, neg, mag, dot, plus, sp, and printMe.

---
+++ MathVector/MathVector.java
@@ -0,0 +1,7 @@
+// Author: 		Justino Mora
+// Date: 		April, 2015
+// Project: 	MathVector
+// Description: MathVector is a class for N-dimensional mathematical (Euclidean) vectors.
+//				Read README file for more details. 
+
+// package MathVector; 
